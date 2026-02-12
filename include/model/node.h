b8c17e42e25272561f9645e7ed4412bc4867acbf
thtran97 <trantronghieu97@gmail.com>
2026-02-12 16:33:03 +0100

    refactor: enhance Node & Problem interface with scaling methods

--- include/model/node.h
+++ include/model/node.h
@@ -9,12 +9,16 @@ namespace oplib::model {
  * @brief Represents a node (customer or depot) in the problem.
  */
 struct Node {
-    NodeId id;
+    NodeId id = -1;
     double x = 0.0;
     double y = 0.0;
     Reward reward = 0.0;
     Time service_time = 0.0;
-    TimeWindow tw = {0.0, 1e18};
+    TimeWindow tw;
+
+    // Preprocessing data: list of (neighbor_id, heuristic_value)
+    // Used by constructive heuristics and local search.
+    std::vector<std::pair<NodeId, double>> neighbors;
 
     std::string to_string() const {
         return "Node " + std::to_string(id) + " (" + std::to_string(x) + "," + std::to_string(y) + ") R=" + std::to_string(reward);
--- include/model/problem.h
+++ include/model/problem.h
@@ -24,9 +24,18 @@ public:
     virtual NodeId get_source_depot() const = 0;
     virtual NodeId get_sink_depot() const = 0;
 
+    // Scaling and Precision
+    virtual ScalingMode get_scaling_mode() const { return ScalingMode::RAW; }
+    virtual double get_time_scale() const { return 1.0; }
+    bool is_scaled() const { return get_scaling_mode() == ScalingMode::SCALED_INTEGER; }
+
     // Rewards and Costs
     virtual Reward get_reward(NodeId i) const = 0;
     virtual Distance get_distance(NodeId i, NodeId j) const = 0;
+
+    // Constraints & Metadata
+    virtual const TimeWindow& get_time_window(NodeId i) const = 0;
+    virtual Time get_service_time(NodeId i) const = 0;
     
     // Abstract check for problem-specific constraints
     // This allows checkers/solvers to work with base types
@@ -38,6 +47,11 @@ public:
     // Default is time-independent, which does not use departure_time. Time-dependent variants should override this.
     virtual Time get_travel_time(NodeId i, NodeId j, Time /*departure_time*/ = 0.0) const {
         return get_distance(i, j); 
+    }
+
+    // Inverse of travel time: find latest departure at i to arrive at j by arrival_time_at_j
+    virtual Time estimate_departure_time(NodeId i, NodeId j, Time arrival_time_at_j) const {
+        return arrival_time_at_j - get_travel_time(i, j);
     }
 };
 
