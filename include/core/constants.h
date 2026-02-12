8bd3661202c89097022716878e95cc36a2716594
thtran97 <trantronghieu97@gmail.com>
2026-02-12 14:44:12 +0100

    feat: add constants and random utility classes for algorithm parameters and random number generation

---
+++ include/core/constants.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "types.h"
+#include <climits>
+
+namespace oplib::constants {
+
+// Numeric Constants
+inline constexpr NodeId INVALID_NODE = -1;
+inline constexpr Reward UNSET_REWARD = -1.0;
+inline constexpr Distance INF_DISTANCE = 1e18; // Use a large double instead of INT_MAX for doubles
+inline constexpr Time INF_TIME = 1e18;
+
+// Default Algorithm Parameters
+inline constexpr int DEFAULT_SEED = 42;
+inline constexpr int DEFAULT_RCL_SIZE = 5;
+inline constexpr double DEFAULT_REMOVAL_RATIO = 0.4;
+inline constexpr int DEFAULT_ALPHA = 2;
+inline constexpr int MAX_NB_VEHICLES = 10;
+
+} // namespace oplib::constants
